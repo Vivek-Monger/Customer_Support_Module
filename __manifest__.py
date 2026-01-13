62cf8a538254c88c47c11be78b77f016abb86bfe
Vivek Saru <vivekmonger2019@gmail.com>
2026-01-13 13:35:04 +0600

    Update FAQ

--- __manifest__.py
+++ __manifest__.py
@@ -51,6 +51,8 @@ Long description of module's purpose
             'customer_support_module/static/src/css/overview_dashboard.css',
             'customer_support_module/static/src/css/navbar.css',
             'customer_support_module/static/src/css/list.css',
+            'customer_support_module/static/src/js/faq_dashboard.js',
+            'customer_support_module/static/src/xml/faq_dashboard.xml',
         ],
     }
 
--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,3 +1,4 @@
 from . import controllers
 from . import portal
 from . import portal_ticket_confirmation
+from . import faq_controllers
